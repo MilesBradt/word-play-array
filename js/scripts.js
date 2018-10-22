024cb1c2d7a5fd9268173bf1301d200be6d4e3ec
Michael Bradt and Ngan Nguyen <ngan@>
2018-10-22 13:32:17 -0700

    take user sentence and split it into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+// Working array
+var sentenceArray = [];
+
 $(document).ready(function() {
-  
+  $(".userInput").submit(function(event){
+    var sentence = $("input#userSentence").val();
+    // Working array
+    // sentenceArray.push(sentence);
+
+    var words = sentence.split(' ');
+    sentenceArray.push(words);
+    console.log(sentenceArray);
+
+    // $(".output").text($("input#userSentence").val());
+
+    // Working array
+    // console.log(sentenceArray);
+
+
+    event.preventDefault();
+  });
 });

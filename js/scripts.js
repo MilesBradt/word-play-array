87bcb8cddceffc2f476d7a9ad9027be4b69325ea
Michael Bradt and Ngan Nguyen <ngan@>
2018-10-22 16:23:27 -0700

    filter user sentence, display revered words that are >= 3, and create reduce list

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,39 @@ $(document).ready(function() {
     var words = sentence.split(' ');
     var test = [];
 
-    words.forEach(function(word) {
-      if (word.length >= 3) {
-        test.push(word);
+    // forEach
+    // words.forEach(function(word) {
+    //   if (word.length >= 3) {
+    //     test.push(word);
+    //   }
+    // });
+
+
+    // Filter
+    const filterWords = words.filter(word => word.length >= 3);
+
+    var reverseFilter = filterWords.reverse();
+    var countedRFilter = reverseFilter.reduce(function(allWords, reverse) {
+      if (reverse in allWords) {
+        allWords[reverse]++;
       }
-    });
+      else {
+        allWords[reverse] = 1;
+      }
+      return allWords;
+    }, {});
 
-    var reverse = test.reverse().toString();
-    $(".output").text(reverse);
 
-    console.log(test);
-    console.log(reverse);
+    console.log(countedRFilter);
+    $(".output").text(reverseFilter.join(' '));
+
+    // $(".output").text(list);
+    // var reverse = test.reverse();
+    //
+    // $(".output").text(reverse.join(' '));
+    //
+    // console.log(test);
+    // console.log(reverse);
     event.preventDefault();
   });
 });

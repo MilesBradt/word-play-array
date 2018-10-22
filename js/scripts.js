c601cde26910c58d00ea61f725dd7924974d290a
Michael Bradt and Ngan Nguyen <michael@>
2018-10-22 14:40:36 -0700

    output and reverse words in a suer sentance that are equal to or greater than 3 characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,21 @@
-// Working array
-// var sentenceArray = [];
-
-
 $(document).ready(function() {
   $(".userInput").submit(function(event){
     var sentence = $("input#userSentence").val();
-    // Working array
-    // sentenceArray.push(sentence);
 
-    // Array we want to change
     var words = sentence.split(' ');
     var test = [];
 
     words.forEach(function(word) {
-      if (word.length > 4) {
+      if (word.length >= 3) {
         test.push(word);
       }
     });
 
-    // // Array we loop through
-    // var cloneWords = words.slice();
-
-
-    // var threeChar = words.map(function(word){
-    //   return word.length > 4;
-    // });
-
-
-
+    var reverse = test.reverse().toString();
+    $(".output").text(reverse);
 
     console.log(test);
-
-    // $(".output").text($("input#userSentence").val());
-
-    // Working array
-    // console.log(sentenceArray);
-
-
+    console.log(reverse);
     event.preventDefault();
   });
 });
